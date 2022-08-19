10fd04c7d182b42d69fde2e834c01f0ab748ecba
umeshkhanna <umeshkhannan@gmail.com>
2022-08-19 18:16:40 +0530

    shared library get and validate token completed

---
+++ oauth/errors/errors.go
@@ -0,0 +1,40 @@
+package errors
+
+import (
+	"errors"
+	"net/http"
+)
+
+type RestErr struct {
+	Message string `json:"message"`
+	Status  int    `json:"status"`
+	Error   string `json:"error"`
+}
+
+func NewError(msg string) error {
+	return errors.New(msg)
+}
+
+func NewBadRequestError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusBadRequest,
+		Error:   "Bad Request",
+	}
+}
+
+func NewNotFoundError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusNotFound,
+		Error:   "Not Found",
+	}
+}
+
+func NewInternalServerError(message string) *RestErr {
+	return &RestErr{
+		Message: message,
+		Status:  http.StatusInternalServerError,
+		Error:   "Internal Server Error",
+	}
+}
